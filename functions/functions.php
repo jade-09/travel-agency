47b2b425f627df64ef1a1e7af49ca4721c121180
jade-09 <81950117+jade-09@users.noreply.github.com>
2023-12-07 03:19:02 +0530

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "tagency");
+$con = mysqli_connect("sreesql.mysql.database.azure.com", "sreenivas", "jade@123", "tagency");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
